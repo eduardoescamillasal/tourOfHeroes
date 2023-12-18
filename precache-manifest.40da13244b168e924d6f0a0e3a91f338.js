e673cc39e2c79708e441bb168f180e8ac65c0127
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2023-12-18 13:56:06 -0600

    Updates

---
+++ precache-manifest.40da13244b168e924d6f0a0e3a91f338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559f772e65be4463bf77abd722e5f319",
+    "url": "/tourOfHeroes/index.html"
+  },
+  {
+    "revision": "49171ac64b667f1b55b9",
+    "url": "/tourOfHeroes/static/css/main.38397bfd.chunk.css"
+  },
+  {
+    "revision": "1ff1c05d6c75ece98eb3",
+    "url": "/tourOfHeroes/static/js/2.35714301.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/tourOfHeroes/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "49171ac64b667f1b55b9",
+    "url": "/tourOfHeroes/static/js/main.b0994dd7.chunk.js"
+  },
+  {
+    "revision": "bc78287ca733eac3bf96",
+    "url": "/tourOfHeroes/static/js/runtime~main.244492da.js"
+  }
+]);
